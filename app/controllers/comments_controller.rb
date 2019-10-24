0cfd6874429072617acb86eee14299f4237a6f5a
mazenswar <mazenswar@protonmail.com>
2019-10-24 13:12:28 -0400

    v2

--- app/controllers/comments_controller.rb
+++
@@ -1,8 +0,0 @@
-class CommentsController < ApplicationController
-    def create
-        comment = Comment.create(comment_params)
-        note = Note.find(comment_params['note_id'])
-        render json: note
-    end
-    
-end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -28,6 +28,6 @@ class NotesController < ApplicationController
     private
 
     def note_params
-        params.require(:note).permit(:title, :content)
+        params.permit(:title, :content)
     end
 end
